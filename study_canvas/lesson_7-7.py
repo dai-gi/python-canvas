b4aa01aea62549958784638c11ede2a28c139404
daisuke524 <daisuke.u524@gmail.com>
2020-12-20 11:12:05 +0900

    修正

--- study_canvas/lesson_7-7.py
+++ study_canvas/lesson_7-7.py
@@ -1,8 +1,10 @@
+# いちばんやさしいPython入門教室
 # 円をディクショナリとリストで管理する
 # coding:utf-8
+
 import tkinter as tk
 
-# 円をリストで用意する
+# 円をコレクション(リストとディクショナリ)で管理
 balls = [
     {"x" : 400, "y" : 300, "dx" : 3, "dy" : 3, "color":"red"},
     {"x" : 200, "y" : 100, "dx" : -3, "dy" : 3, "color":"green"},
@@ -13,36 +15,34 @@ balls = [
 def move():
     global balls
     for b in balls:
-        # いまの円を消す
-        canvas.create_oval(b["x"] - 30, b["y"] - 30, b["x"] + 30, b["y"] + 30, fill="white",width=0)
+        canvas.create_oval(b["x"] - 20.9, b["y"] - 20.9, b["x"] + 20.9, b["y"] + 20.9, fill="white",width=0)
         # Xの座標を動かす
         b["x"] = b["x"] + b["dx"]
         # Yの座標も動かす
         b["y"] = b["y"] + b["dy"]
-        # 次の位置に円を描く
+        # 円の位置
         canvas.create_oval(b["x"] - 20, b["y"] - 20, b["x"] + 20, b["y"] + 20, fill=b["color"],width=0)
+        
         # 端を超えていたら反対向きにする
+        # x座標
         if b["x"] >= canvas.winfo_width():
-            b["dx"] = -3
+            b["dx"] = -1
         if b["x"]  <= 0:
-            b["dx"] = +3
-        # Y座標についても同様
+            b["dx"] = +1
+        # y座標
         if b["y"] >= canvas.winfo_height():
-            b["dy"] = -3
+            b["dy"] = -1
         if b["y"] <= 0:
-            b["dy"] = +3
-    # 再びタイマー
+            b["dy"] = +1
+
     root.after(10, move)
 
-# ウィンドウを描く
 root = tk.Tk()
 root.geometry("600x400")
 
-# キャンバスを置く
 canvas = tk.Canvas(root,width =600, height =400, bg="white")
 canvas.place(x = 0, y = 0)
 
-# タイマーを設定する
 root.after(10, move)
 
 root.mainloop()
